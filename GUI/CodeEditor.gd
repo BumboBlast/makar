4553850ca2e2184ec16a8046ac17d3fee5884739
BumboBlast <john.jameschar@gmail.com>
2022-12-10 16:15:05 -0500

    added editor  syntax highlighting

--- GUI/CodeEditor.gd
+++ GUI/CodeEditor.gd
@@ -4,5 +4,11 @@ extends TextEdit
 
 
 func _ready():
+	add_keyword_color("make", ColorN("lightblue"))
+	add_keyword_color("array", ColorN("lightblue"))
+	add_keyword_color("step", ColorN("lightblue"))
 	pass
 
+func _input(event):
+	if (event.is_action_pressed("ui_accept")):
+		pass
