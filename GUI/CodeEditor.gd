1d4bcd4982ed915ba8f880ac03a52396e3804630
BumboBlast <john.jameschar@gmail.com>
2022-12-10 17:10:20 -0500

    editor text is passed to parser

--- GUI/CodeEditor.gd
+++ GUI/CodeEditor.gd
@@ -11,4 +11,10 @@ func _ready():
 
 func _input(event):
 	if (event.is_action_pressed("ui_accept")):
-		pass
+		push_code()
+
+
+func push_code():
+	var editor_content: String = self.get_text();
+	get_tree().get_root().get_node("Application/MakarPsuedoCode").set_editor_code(editor_content)
+	get_tree().get_root().get_node("Application/GUI").emit_signal("try_execute")
